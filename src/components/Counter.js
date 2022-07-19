55957d65602b2635f35eb9d93b288e10b62d5b8c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-19 16:09:40 +0400

    Introduce dispatch function using useDispatch react-redux hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,8 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter =  useSelector(state => state.counter);
 
   const toggleCounterHandler = () => {};
