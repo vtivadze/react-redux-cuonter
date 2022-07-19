67f4fe4b8e1baaf7e33fd07efa86cf1c9460ed7c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-19 16:53:58 +0400

    Add button for inceasing counter by 5

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,6 +8,9 @@ const Counter = () => {
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
+  const increaseBy5Handler = () => {
+    dispatch({ type: 'increaseBy5'});
+  };
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
@@ -20,6 +23,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseBy5Handler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,12 @@ const counterReducer = (state = {counter: 0}, action) => {
     }
   }
 
+  if (action.type === 'increaseBy5') {
+    return {
+      counter: state.counter + 5,
+    }
+  }
+
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
