c806400a9558a3528e51b3e4ef3eb69026c179c7
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-19 16:12:09 +0400

    Define dispatches

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,13 @@ const Counter = () => {
   const dispatch = useDispatch();
   const counter =  useSelector(state => state.counter);
 
+  const incrementHandler = () => {
+    dispatch({type: 'increment'});
+  };
+  const decrementHandler = () => {
+    dispatch({type: 'decrement'});
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -12,8 +19,8 @@ const Counter = () => {
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
-        <button>Increment</button>
-        <button>Decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
